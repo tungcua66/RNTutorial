8e5431e732b82dd80b69065ab20cabb39fb1d30e
tungcua66 <tunghuong66bg@gmail.com>
2023-03-17 16:32:19 +0100

    clear all the base project

--- App.js
+++ App.js
@@ -1,59 +1,17 @@
-import { useState } from 'react';
-import {
-  StyleSheet, View, FlatList, Button
-} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-import GoalInput from './components/GoalInput';
-import GoalListItem from './components/GoalListItem';
-
 export default function App() {
-  const [goalList, setGoalList] = useState([]);
 
-  const [modalIsVisible, setModalIsVisible] = useState(false);
   return (
-    <>
-      <StatusBar style="inverted" />
-      <View style={styles.mainContainer}>
-        <Button
-          title="Add new goal"
-          onPress={() => { setModalIsVisible(true); }}
-          color="#1d7874"
-        />
-        <GoalInput
-          setGoalList={setGoalList}
-          modalIsVisible={modalIsVisible}
-          setModalIsVisible={setModalIsVisible}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            alwaysBounceVertical={false}
-            data={goalList}
-            renderItem={(itemData) => {
-              return (
-                <GoalListItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  setGoalList={setGoalList}
-                />
-              );
-            }}
-            keyExtractor={(item) => { return item.id; }}
-          />
-        </View>
-      </View>
-    </>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    margin: 50,
-    marginHorizontal: 16
-  },
-  goalsContainer: {
-    flex: 5,
-  },
 
 });
