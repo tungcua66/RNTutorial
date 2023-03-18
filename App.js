ca2a053cf9525889c2eea14e24ba81c03e510511
tungcua66 <tunghuong66bg@gmail.com>
2023-03-18 23:13:17 +0100

    improve imageBackgound

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 import StartGameScreen from './screens/StartGameScreen';
+
+const backgroundImage = require('./assets/images/background.jpg');
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['#172b94', '#62c2dd']}
+      style={styles.container}
+    >
       <StatusBar style="auto" />
-      <StartGameScreen />
-    </View>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.container}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
 });
