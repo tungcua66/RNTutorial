9701e67209138c1bed64f592ea7e2361768d76a3
tungcua66 <tunghuong66bg@gmail.com>
2023-03-18 20:42:08 +0100

    adding feedback to pressed button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, Pressable
+} from 'react-native';
 
 const PrimaryButton = ({ text }) => {
   return (
-    <View>
-      <Text style={styles.text}>{text}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => (pressed
+          ? [styles.pressed, styles.buttonInnerContainer] : styles.buttonInnerContainer)}
+        android_ripple={{ color: '#5293ea', borderless: true }}
+      >
+        <Text style={styles.buttonText}>{text}</Text>
+      </Pressable>
     </View>
   );
 };
@@ -11,7 +19,23 @@ const PrimaryButton = ({ text }) => {
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#81e5d8'
+  buttonOuterContainer: {
+    backgroundColor: '#172b94',
+    marginHorizontal: 30,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonInnerContainer: {
+    padding: 10,
+    width: '100%',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  },
+  pressed: {
+    opacity: 0.70,
   }
 });
